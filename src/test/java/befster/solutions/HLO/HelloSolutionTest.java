2c3f9b54707767919288668c6d0c2d777262bbe6
sbx_user1060 <sbx_user1060@ip-10-128-120-52.eu-west-2.compute.internal>
2018-12-08 13:59:51 +0000

    Sat Dec 08 13:59:51 UTC 2018

---
+++ src/test/java/befster/solutions/HLO/HelloSolutionTest.java
@@ -0,0 +1,24 @@
+package befster.solutions.HLO;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import befaster.solutions.HLO.HelloSolution;
+
+public class HelloSolutionTest {
+
+	HelloSolution helloSolution;
+	@Before
+	public void setUp() throws Exception {
+		
+		helloSolution = new HelloSolution();
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
