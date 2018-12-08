8172bff60160681dfa73bd876dab8376aa8db323
sbx_user1060 <sbx_user1060@ip-10-128-120-52.eu-west-2.compute.internal>
2018-12-08 14:49:58 +0000

    Sat Dec 08 14:49:58 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -1,9 +1,14 @@
 package befaster.solutions.FIZ;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FizzBuzzSolution {
 
 	public String fizzBuzz(Integer number) {
+		if (isFizz(number) && isBuzz(number) && isDeluxe(number))
+			return "fizz buzz deluxe";
 		if (isFizz(number) && isBuzz(number))
 			return "fizz buzz";
 		if (isFizz(number))
@@ -21,6 +26,12 @@ public class FizzBuzzSolution {
 	private boolean isBuzz(Integer number) {
 		return ( (number % 5) == 0) || (String.valueOf(number).indexOf("5")>=0);
 	}
+	
+	private boolean isDeluxe (Integer number) {
+	Set s = new HashSet(Arrays.asList(String.valueOf(number).split("")));
+	return (s.size() == 1);
+	}
 
 }
+
 
