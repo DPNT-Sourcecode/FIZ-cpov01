e50aca49c468233458cd9a4f84db48dd7c55f175
sbx_user1060 <sbx_user1060@ip-10-128-120-52.eu-west-2.compute.internal>
2018-12-08 14:17:49 +0000

    Sat Dec 08 14:17:49 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -1,19 +1,30 @@
 package befaster.solutions.FIZ;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzSolutionTest {
-
+  
+	FizzBuzzSolution fizzBuzzSolution;
 	@Before
 	public void setUp() throws Exception {
+		
+		fizzBuzzSolution  = new FizzBuzzSolution ();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFizz() {
+		//given
+		int input = 3;
+		//when 
+		String actual = fizzBuzzSolution.fizzBuzz(3);
+		//then
+		assertThat("fizz",is(equalTo(actual)));
 	}
 
 }
+
